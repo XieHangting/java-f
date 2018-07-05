03f6850ead0b7193b72c26fd09e9f2f6bf4689d3
Your Name <you@example.com>
2018-07-05 20:45:41 +0800

    init

--- src/MilkTest.java
+++ src/MilkTest.java
@@ -1,13 +1,22 @@
 public class MilkTest {
     public static void main(String[] args) {
         String type = "pure";
+        Milk[] milks = new Milk[3];
 
         Milk a = new Milk("蒙牛",type,250,2018,24);
-        Milk b = new Milk("伊利",type,250,2018,24);
-        Milk c = new Milk("蒙牛",type,250,2018,24);
+        Milk b = new Milk("伊利",type,222,2018,24);
+        Milk c = new Milk("蒙牛",type,255,2017,24);
 
-        System.out.println(a.getBrand()+" | "+a.getType()+" | "+a.getVolume()+" | "+a.getDataInProduced()+" | "+a.getShelfLife());
-        System.out.println(a.getBrand()+" | "+b.getType()+" | "+b.getVolume()+" | "+b.getDataInProduced()+" | "+b.getShelfLife());
-        System.out.println(a.getBrand()+" | "+c.getType()+" | "+c.getVolume()+" | "+c.getDataInProduced()+" | "+c.getShelfLife());
+        milks[0] = a;
+        milks[1] = b;
+        milks[2] = c;
+
+        System.out.println("有牛奶"+milks.length+"瓶，他们是");
+
+        for (Milk milk : milks) {
+            System.out.println(milk.getBrand()+" | "+milk.getType()+" | "+milk.getVolume()+" | "+milk.getDataInProduced()+" | "+milk.getShelfLife());
+        }
+
+
     }
 }
--- src/TreadmillTest.java
+++ src/TreadmillTest.java
@@ -1,11 +1,25 @@
+import java.util.ArrayList;
+
 public class TreadmillTest {
     public static void main(String[] args) {
        Treadmill a = new Treadmill("1号",2018,"尼龙",23,"blue","2018-1",2000);
        Treadmill b = new Treadmill("2号",2017,"不锈钢",20,"red","2017-1",2050);
        Treadmill c = new Treadmill("3号",2018,"某某",24,"yellow","2018-2",2200);
+       Treadmill d = new Treadmill("4号",2016,"某某某",26,"black","2016-1",2222);
 
-        System.out.println(a.getBrand()+" | "+a.getPublishTime()+" | "+a.getMaterial()+" | "+a.getFloorSpace()+" | "+a.getColor()+ " | "+a.getProductCode()+" | "+a.getPower());
-        System.out.println(b.getBrand()+" | "+b.getPublishTime()+" | "+b.getMaterial()+" | "+b.getFloorSpace()+" | "+b.getColor()+ " | "+b.getProductCode()+" | "+b.getPower());
-        System.out.println(c.getBrand()+" | "+c.getPublishTime()+" | "+c.getMaterial()+" | "+c.getFloorSpace()+" | "+c.getColor()+ " | "+c.getProductCode()+" | "+c.getPower());
+        ArrayList<Treadmill> treadmills = new ArrayList();
+
+        treadmills.add(a);
+        treadmills.add(b);
+        treadmills.add(c);
+        treadmills.add(d);
+
+        System.out.println("我有跑步机"+treadmills.size()+"架，分别是:");
+
+        for (Treadmill treadmill : treadmills) {
+            System.out.println(treadmill.getBrand()+" | "+treadmill.getPublishTime()+" | "+treadmill.getMaterial()+" | "+treadmill.getFloorSpace()+" | "+treadmill.getColor()+ " | "+treadmill.getProductCode()+" | "+treadmill.getPower());
+        }
+
+
     }
 }
